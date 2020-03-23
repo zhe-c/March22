7e29a69d7bc87050d340b34da11af8edca6bbd10
zhe-c <62517384+zhe-c@users.noreply.github.com>
2020-03-22 21:15:06 -0300

    Update main.js

--- flightmap/main.js
+++ flightmap/main.js
@@ -1,13 +1,5 @@
 (function(){
-	document.onkeydown=function (e){
-		var currKey=0,evt=e||window.event;
-		currKey=evt.keyCode||evt.which||evt.charCode;
-		if (currKey == 123) {
-			window.event.cancelBubble = true;
-			window.event.returnValue = false;
-		}
-	}
-	
+
     //create map in leaflet and tie it to the div called 'theMap'
     var map = L.map('theMap').setView([42, -60], 4);
 
